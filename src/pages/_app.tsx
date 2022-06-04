dec8badfefd530b231bf3dbd6c93a9e452e3b113
HashRei <hashrei@protonmail.com>
2022-06-04 16:14:04 +0200

    Add web3-react, next-themes and motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { ThemeProvider } from "next-themes"
+import { motion } from "framer-motion"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { Web3ReactProvider } from "@web3-react/core"
+import { ExternalProvider, JsonRpcFetchFunc, Web3Provider } from "@ethersproject/providers"
+
+function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
+  return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+}
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        }
+      }}
+    >
+      <ThemeProvider attribute="class">
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Component {...pageProps} />
+        </Web3ReactProvider>
+      </ThemeProvider>
+    </motion.div>
+  )
 }
 
 export default MyApp
